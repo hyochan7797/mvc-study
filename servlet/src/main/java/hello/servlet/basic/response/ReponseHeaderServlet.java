897088a3ced9b7433a70557f5cb44932a328376c
hyochan7797 <gyskfen17@naver.com>
2026-01-29 22:00:04 +0900

    Add files via upload

---
+++ servlet/src/main/java/hello/servlet/basic/response/ReponseHeaderServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name ="responseHeaderServlet", urlPatterns = "/response-header")
+public class ReponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        resp.setHeader("Content-Type", "text/plain;charset=UTF-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("안녕");
+    }
+    }
